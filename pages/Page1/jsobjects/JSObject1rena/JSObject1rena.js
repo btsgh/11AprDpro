3cd203bd6e98fe910fdd0c1c246a68513263cace
Aparna Ramachandran <aparna@appsmith.com>
2024-08-02 08:27:52 +0000

    commit

---
+++ pages/Page1/jsobjects/JSObject1rena/JSObject1rena.js
@@ -0,0 +1,12 @@
+export default {
+	myVasr1: [],
+	myVarssss2: {},
+	myFusssn1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
